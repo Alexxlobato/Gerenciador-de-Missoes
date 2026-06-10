fca2c33464ac96d31eb11a3322e11758995f58a1
Alex <alex@copinhodobem.com.br>
2026-06-10 07:47:21 -0300

    Feat: Adicionar missão, salvar no locarlSrorage, Adicionar Lista, Concluir mmissão e remover missão

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 let missoes = JSON.parse(localStorage.getItem("missoesDoUsuario")) || [];
 
+// Elementos do HTML que o JavaScript vai controlar
+const inputMissao = document.getElementById("missionInput");
+const botaoAdicionar = document.getElementById("addMissionBtn");
+const botaoSortear = document.getElementById("drawMissionsBtn");
+const listaUl = document.getElementById("missionsList")
+
+// Elementos das Estatísticas
+const totalMissions = document.getElementById("totalMissions");
+const completeMissions = document.getElementById("completeMissions");
+const pendentMissions = document.getElementById("pendentMissions")
+
+// Elemento do Sorteio
+const drawMissionText = document.getElementById("drawMissionText")
+
+// Função para salvar a lista de missões na gaveta "missoesDoUsuario"
+function salvarNoLocalStorage() {
+    try { 
+        localStorage.setItem("missoesDoUsuario", JSON.stringify(missoes));
+    } catch (error) { 
+        console.error("Erro ao salvar no localstorage:", error)}
+    }
+        
+// Função para adicionar uma nova missão
 function adicionarMissao() {
 
-    const missaoCapturadanoInput = document.getElementById("missionInput").value.trim();
+    const missaoCapturadanoInput = inputMissao.value.trim();
     
     if (missaoCapturadanoInput === "") {
         alert("Digite uma missão para adicionar!"); 
@@ -14,46 +37,48 @@ function adicionarMissao() {
         concluida: false
         };
 
-    missoes.push(novaMissao);       
+    missoes.push(novaMissao); // Limpa o campo de texto
     inputMissao.value = "";     
+
+    salvarNoLocalStorage();
+    atualizarLista();
       
     }
 
 function atualizarLista() {
        
-    const lista = document.getElementById("missionsList");
-    lista.innerHTML = "";
+    listaUl.innerHTML = "";
 
-    missoes.forEach(function(missao) {
+    missoes.forEach(function(missao, index){
     const li = document.createElement("li");
-        li.textContent = missao.nome;
-        if (missao.concluida) {
-            li.innerHTML += "(✅ Concluída)";
-        } else {
-                li.innerHTML += "(☐ Pendente)";
-            }
-        lista.appendChild(li);
-            }
+    li.className = "missionItem";
+       
+    const span = document.createElement("span");
+    span.className = "missionText";
+    span.textContent = missao.nome;
 
-         );
-
-    }
-
-function salvarNoLocalStorage() {
-    try { 
-        localStorage.setItem("missoesDoUsuario", JSON.stringify(missoes));
-    } catch (error) { 
-        console.error("Erro ao salvar no localstorage:", error)}
+    li.appendChild(span)
+        
+    if (missao.concluida) {
+        span.textContent += "(✅ Concluída)";
+    } else {
+        span.textContent += "(☐ Pendente)";
     }
         
-   
-
-function concluirMissao() {
-    const missaoClicada = document.getElementById("missionItem");
-    if (missao.concluida) {
-
-    }
+    listaUl.appendChild(li);
     
-    
+    }); 
+} 
+
+function concluirMissao(indice) {
+    missoes[indice].concluida = !missoes[indice].concluida;
+    salvarNoLocalStorage();
+    atualizarLista();
+}
+
+function removerMissao(indice){
+    missoes.splice (indice,1)
+    salvarNoLocalStorage();
+    atualizarLista();
 }
 
